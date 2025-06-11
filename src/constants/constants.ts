7a4703ba504504ce2f173429b8afdfba3c81154d
stephenzac <stephenzacarias2@gmail.com>
2025-06-11 12:24:46 -0700

    feat: create game state constants and types

---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const GAME_BOARD_NUM_ROWS = 20;
+export const GAME_BOARD_ROW_LENGTH = 20;
---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+/**
+ * Types used to help model the Tetris game state.
+ */
+
+/**
+ * The entire game board containing the game "pieces".
+ */
+export type GameBoard = Array<Array<number>>;
+
+/**
+ * The randomly selected piece that "falls" with every tick,
+ * and is controlled by the player.
+ */
+export type CurrentPiece = Array<Array<number>>;
