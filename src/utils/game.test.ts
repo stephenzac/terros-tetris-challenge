d0eb6f7e48b8c674a730e5eb1ac68e7ad0a25937
stephenzac <stephenzacarias2@gmail.com>
2025-06-11 13:02:27 -0700

    chore: add unit tests

--- src/utils/game.test.ts
+++ src/utils/game.test.ts
@@ -11,15 +11,26 @@ describe('test game functions', () => {
   test('game board creation', () => {
     const gameBoard = createGameBoard();
 
+    // Check correct game board size
     expect(gameBoard.length).toBe(GAME_BOARD_NUM_ROWS);
     expect(gameBoard[0].length).toBe(GAME_BOARD_ROW_LENGTH);
+
+    // Check correct game board value initialization
+    gameBoard.forEach((row) => {
+      expect(row).toEqual(Array(GAME_BOARD_ROW_LENGTH).fill(0));
+    });
   });
 
   test('random piece creation', () => {
     const randomPiece = generateRandomPiece();
 
-    // Even with randomness, every piece should fit these size constraints
+    // Even with randomness, every possible piece should fit these size constraints
     expect(randomPiece.length).toBeGreaterThan(0);
     expect(randomPiece.length).toBeLessThan(4);
+  });
+
+  test('game tick logic', () => {
+    const gameBoard = createGameBoard();
+    const randomPiece = generateRandomPiece();
   });
 });
